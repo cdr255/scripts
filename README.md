1dbccbea3c3997d42231efd03871b2d2b364c23e
Christopher Rodriguez <cdr255@gmail.com>
2017-07-05 15:18:39 -0400

    Initial Commit, README.md and mpdburn.sh

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scripts Repository
+
+This is my personal repository of scripts I use on all of my systems.
---
+++ mpdburn.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo "Opening Current MPD playlist in k3b for burning...";
+
+currtime=$(date +"%F_%T");
+
+echo \#EXTM3U > $currtime-mpdburn-playlist.m3u;
+
+mpc -f "##EXTINF:%time%,%artist% - %title%\n/home/chrissofer/Music/%file%" playlist | perl -F: -p -e 'if (/^#EXTINF/) {$sec=$F[1]*60; $F[2] =~ s/(^.+),/$1/; $sec += $F[2]; s/^.+,(.+)/#EXTINF:$sec,$1/;}' >> $currtime-mpdburn-playlist.m3u;
+
+k3b --audiocd $currtime-mpdburn-playlist.m3u --nosplash
+
+kdialog --yesno "Delete Temporary Extended-M3U playlist?" 
+
+if [ "$?" == 0 ]; then
+    echo "Okay, Deleting $currtime-mpdburn-playlist.m3u"
+    rm -v $currtime-mpdburn-playlist.m3u;
+elif [ "$?" == 1 ]; then
+    echo "Okay, Keeping $currtime-mpdburn-playlist.m3u"
+else
+    kdialog --error "Unexpected Choice."
+fi
